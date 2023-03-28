f39fef88eacafb82c208177a2afedebcd438f292
jokim <jokim@hecto.co.kr>
2023-03-28 15:07:11 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/projectrebase/ProjectRebaseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.projectrebase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectRebaseApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectRebaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/projectrebase/ProjectRebaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.projectrebase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectRebaseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
